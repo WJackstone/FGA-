ee783ea74d5a9c7e8aa111c1fca2e6b8e81efba3
WJackstone <45036362+WJackstone@users.noreply.github.com>
2018-11-16 10:45:55 -0200

    Add files via upload

---
+++ tabs/TelaPerfil.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Text, View, Button, Image } from 'react-native';
+import { AppRegistry } from 'react-native';
+
+export default class TelaPerfil extends React.Component {
+  static navigationOptions = {
+    tabBarLabel: 'Tab2'
+  }
+
+  render() {
+    return <View style={
+        {
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }
+    }>
+      <Text style={{fontSize: 30}}>
+        Tela de perfil a ser implementada
+      </Text>
+    </View>
+  }
+}
