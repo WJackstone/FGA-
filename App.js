94a7a900e2f814490c4c164b476362c4cabf99be
Daniel de Andrade Souza <daniel.android.andrade@gmail.com>
2018-11-16 21:37:25 -0200

    tabBar atualizada

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-import { createBottomTabNavigator } from "react-navigation";
+import { TabNavigator } from "react-navigation";
 import TelaGf from './tabs/TelaGf'
 import TelaPerfil from './tabs/TelaPerfil'
 
-var MainScreenNavigator = createBottomTabNavigator({
-  tab1: {screen: TelaPerfil},
-  tab2: {screen: TelaGf}
-
+var MainScreenNavigator = TabNavigator({
+  tab1: {screen: TelaGf},
+  tab2: {screen: TelaPerfil}
+}, {
+  tabBarPosition: 'bottom',
+  swipeEnabled: true,
+  tabBarOptions: {
+    activeTintColor: 'white',
+    activeBackgroundColor: 'darkgreen',
+    inactiveTintColor: 'black',
+    inactiveBackgroundColor: 'green',
+    labelStyle: {
+      fontSize: 16,
+      padding: 0
+    }
+  }
+}
 });
 
 MainScreenNavigator.navigationOptions ={
